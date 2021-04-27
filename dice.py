ea94ad0c273cd26e9d4a68904abb6504f9a47d71
Marceloprime <marceloreis7777@gmail.com>
2021-04-27 16:07:05 -0300

    first commit

---
+++ dice.py
@@ -0,0 +1,40 @@
+import random
+
+number_face = random.randint(1,6)
+
+if number_face == 1:
+    print("---------------")
+    print("-             -")
+    print("-      *      -")
+    print("-             -")
+    print("---------------")
+elif number_face == 2:
+    print("---------------")
+    print("-      *      -")
+    print("-             -")
+    print("-      *      -")
+    print("---------------")
+elif number_face == 3:
+    print("---------------")
+    print("-      *      -")
+    print("-      *      -")
+    print("-      *      -")
+    print("---------------")
+elif number_face == 4:
+    print("---------------")
+    print("-  *        * -")
+    print("-             -")
+    print("-  *        * -")
+    print("---------------")
+elif number_face == 5:
+    print("---------------")
+    print("-   *      *  -")
+    print("-       *     -")
+    print("-   *      *  -")
+    print("---------------")
+elif number_face == 6:
+    print("---------------")
+    print("-   *      *  -")
+    print("-   *      *  -")
+    print("-   *      *  -")
+    print("---------------")
